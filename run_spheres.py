a2fcf4b0436dc04b81121c9a74a9ebe9997306e4
Denys Rozumnyi <denysr@lo-s4-019.leonhard.ethz.ch>
2021-03-26 14:23:48 +0100

    update

--- run_spheres.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-from sphere_settings import g_blender_excutable_path
-
-if __name__ == '__main__':
-    command = [g_blender_excutable_path, '--background', '-noaudio', '--python', 'render_spheres.py']
-    subprocess.run(command)
