554da123312e4477307440dc3b2c6cca79970ede
Denys Rozumnyi <denysr@lo-login-02.leonhard.ethz.ch>
2021-03-27 13:17:46 +0100

    update

--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
-# g_shapenet_path = "/cluster/home/denysr/scratch/dataset/ShapeNetv2/ShapeNetCore.v2.zip"
-g_shapenet_path = "/cluster/home/denysr/scratch/dataset/sphere.zip"
+g_shapenet_path = "/cluster/home/denysr/scratch/dataset/ShapeNetv2/ShapeNetCore.v2.zip"
+# g_shapenet_path = "/cluster/home/denysr/scratch/dataset/sphere.zip"
 g_texture_path = "/cluster/home/denysr/scratch/dataset/ShapeNetv2/textures.zip"
 g_blender_path = r"/cluster/scratch/denysr/src/blender-2.91.0-linux64/blender"
+
+import os 
+if not os.path.exist(g_shapenet_path):
+	g_shapenet_path = "/home/denysr/data/ShapeNetCore.v2.zip"
+	g_texture_path = "/home/denysr/data/textures.zip"
+	g_blender_path = r"/home/denysr/Programs/blender-2.91.0-linux64/blender"
 
 # ensure that blender python has pillow pachage, e.g.
 # /cluster/scratch/denysr/src/blender-2.91.0-linux64/2.91/python/bin/python3.7m -m pip install --target="/cluster/home/denysr/scratch/src/blender-2.91.0-linux64/2.91/python/lib/python3.7/"  pillow
